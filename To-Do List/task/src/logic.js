b46e20a624a22d0e728c172e1dbc1353d0779a35
KotenkoNata <35960689+KotenkoNata@users.noreply.github.com>
2021-11-02 16:32:42 +0200

    add HTML styles and logic

--- To-Do List/task/src/logic.js
+++ To-Do List/task/src/logic.js
@@ -1,21 +1,94 @@
 let addBtn = document.getElementById('add-task-button');
 let inputField = document.getElementById('input-task');
-
 let checkboxInput = document.querySelectorAll('[type = "checkbox"]');
-
 let taskListElement = document.getElementById('task-list');
 
 let taskList = [];
 
+
+//delete data from taskList
+
+const deleteDataTaskList = (id)=>{
+    for(let i=0; i<taskList.length; i++){
+        if ( taskList[i].id === id) {
+            taskList.splice(i, 1);
+        }
+    }
+    saveDataToLocalStorage();
+}
+
+
+
+//create li elements from local storage data
+const createListFromLocalData = (data) =>{
+
+    for(let i=0; i<=data.length-1; i++){
+
+        let newLiElement = document.createElement('li');
+        let newInputElement = document.createElement('input');
+        let newSpanElement = document.createElement('span');
+        let newButtonElement = document.createElement('button');
+
+        newInputElement.type = 'checkbox';
+        newInputElement.onchange = function (e){
+            if(e.path[1].children[1].classList.contains('checked')){
+                e.path[1].children[1].classList.remove('checked');
+            }else{
+                e.path[1].children[1].classList.add('checked');
+            }
+        }
+
+        newSpanElement.className = 'task';
+
+        newButtonElement.innerHTML = 'x';
+        newButtonElement.className = 'delete-btn';
+        newButtonElement.id = `${data[i].id}`
+
+        newButtonElement.onclick=function (e) {
+            deleteDataTaskList(e.path[0].id);
+            e.path[1].remove();
+        }
+
+        let node = document.createTextNode(`${data[i].value}`);
+
+        newLiElement.appendChild(newInputElement);
+        newLiElement.appendChild(newSpanElement);
+        newLiElement.appendChild(newButtonElement);
+
+        newSpanElement.appendChild(node);
+
+        taskListElement.appendChild(newLiElement);
+    }
+}
+
+
+
+//update data from local storage during reloading page
+window.addEventListener('load', (event) => {
+    loadDataFromLocalStorage();
+    createListFromLocalData(taskList);
+});
+
+//create new li - add task button
+
 let onAddNewTask = function (ev) {
 
-    let task = inputField.value;
+    let taskValue = inputField.value;
 
-    if (task !== '') {
+    if (taskValue !== '') {
+
+        //write data to task list
 
         let id = 'id'+(new Date()).getTime();
 
-        saveData(id, inputField.value);
+        const firstObject = {
+            id: id,
+            value: taskValue
+        };
+
+        taskList.push(firstObject);
+
+        saveDataToLocalStorage("tasks", taskList);
 
         let newLiElement = document.createElement('li');
         let newInputElement = document.createElement('input');
@@ -24,16 +97,26 @@ let onAddNewTask = function (ev) {
 
         newInputElement.type = 'checkbox';
 
+        newInputElement.onchange = function (e){
+            if(e.path[1].children[1].classList.contains('checked')){
+                e.path[1].children[1].classList.remove('checked');
+            }else{
+                e.path[1].children[1].classList.add('checked');
+            }
+        }
+
         newSpanElement.className = 'task';
 
         newButtonElement.innerHTML = 'x';
         newButtonElement.className = 'delete-btn';
+        newButtonElement.id = `${id}`;
 
-        newButtonElement.onclick=function (e) {
-                e.path[1].remove();
+        newButtonElement.onclick = function (e) {
+            deleteDataTaskList(e.path[0].id);
+            e.path[1].remove();
         }
 
-        let node = document.createTextNode(`${task}`);
+        let node = document.createTextNode(`${taskValue}`);
 
         newLiElement.appendChild(newInputElement);
         newLiElement.appendChild(newSpanElement);
@@ -49,27 +132,27 @@ let onAddNewTask = function (ev) {
 
 addBtn.onclick = onAddNewTask;
 
-document.querySelectorAll('.delete-btn').forEach((value, i) => {
-    value.addEventListener('click', evt => {
-        evt.path[1].remove()
+//delete item
+
+let callbackfn = (buttonEl) => {
+    buttonEl.addEventListener('click', evt => {
+        deleteDataTaskList(evt.path[0].id);
+        evt.path[1].remove();
     })
-})
+};
 
-checkboxInput.forEach((value, i)=>{
-    value.addEventListener('change', e=>{
-
-        if(e.path[1].children[1].classList.contains('checked')){
-            e.path[1].children[1].classList.remove('checked');
-        }else{
-            e.path[1].children[1].classList.add('checked');
-        }
+document.querySelectorAll('.delete-btn').forEach(callbackfn);
 
 
-    })
-})
+//save data to local storage
 
-const saveData = (key, value) =>{
-    const task = JSON.stringify(value);
-    localStorage.setItem(key, task);
+const saveDataToLocalStorage = () =>{
+    const task = JSON.stringify(taskList);
+    localStorage.setItem("tasks", task);
+};
+
+//load data from local storage
+const loadDataFromLocalStorage = () =>{
+    taskList = JSON.parse(localStorage.getItem("tasks")) || [];
 }
 
